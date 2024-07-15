d739196262eb741f9cdd75b97dee14e465b7fd48
KarolisG <kgaivenis@gmail.com>
2024-07-15 21:53:23 +0300

    feat: add find by id method in grocery list controller

---
+++ server/src/controllers/groceryList/findById.ts
@@ -0,0 +1,24 @@
+import { publicProcedure } from '@server/trpc'
+import { TRPCError } from '@trpc/server'
+import provideRepos from '@server/trpc/provideRepos'
+import { groceryListRepository } from '@server/repositories/groceryListRepository'
+import { groceryListSchema } from '@server/entities/groceryList'
+
+export default publicProcedure
+  .use(
+    provideRepos({
+      groceryListRepository,
+    })
+  )
+  .input(groceryListSchema.pick({ id: true }))
+  .query(async ({ input, ctx: { repos } }) => {
+    const result = await repos.groceryListRepository.findById(input.id)
+
+    if (!result) {
+      throw new TRPCError({
+        code: 'NOT_FOUND',
+        message: 'Grocery list not found',
+      })
+    }
+    return result
+  })
---
+++ server/src/controllers/groceryList/tests/findById.spec.ts
@@ -0,0 +1,58 @@
+import { createTestDatabase } from '@tests/utils/database'
+import { createCallerFactory } from '@server/trpc'
+import { wrapInRollbacks } from '@tests/utils/transactions'
+import { insertAll, clearTables } from '@tests/utils/records'
+import { fakeUser, fakeMealPlan } from '@server/entities/tests/fakes'
+import groceryListRouter from '..'
+
+let user: any
+let mealPlan: any
+
+const db = await wrapInRollbacks(createTestDatabase())
+const createCaller = createCallerFactory(groceryListRouter)
+const { findById } = createCaller({ db })
+
+async function createFakeGroceryList() {
+  const list = {
+    mealPlanId: mealPlan.id,
+    product: 'snake oil',
+    quantity: 30,
+  }
+  const [data] = await insertAll(db, 'groceryList', [list])
+  return { id: data.id, ...list }
+}
+
+beforeEach(async () => {
+  ;[user] = await insertAll(db, 'user', [fakeUser()])
+  ;[mealPlan] = await insertAll(db, 'mealPlan', [
+    fakeMealPlan({ userId: user.id }),
+  ])
+})
+
+afterEach(async () => {
+  await clearTables(db, ['groceryList', 'mealPlan', 'user'])
+})
+
+describe('findById', () => {
+  it('should retrieve a grocery list item by ID', async () => {
+    const fakeGroceryList = await createFakeGroceryList()
+    const result = await findById({ id: fakeGroceryList.id })
+
+    expect(result).toEqual(
+      expect.objectContaining({
+        id: fakeGroceryList.id,
+        mealPlanId: fakeGroceryList.mealPlanId,
+        product: fakeGroceryList.product,
+        quantity: fakeGroceryList.quantity,
+      })
+    )
+  })
+
+  it('should throw an error if the grocery list item is not found', async () => {
+    const nonExistentId = 999 // Assuming 999 does not exist in your test data
+
+    await expect(findById({ id: nonExistentId })).rejects.toThrow(
+      /list not found/i
+    )
+  })
+})
