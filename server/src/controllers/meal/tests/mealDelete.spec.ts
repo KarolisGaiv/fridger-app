fb394bd27c18227f50f860aa042f8b5b0282ea21
KarolisG <kgaivenis@gmail.com>
2024-07-17 16:38:40 +0300

    update: meal controller delete method - change to authenticated procedure

--- server/src/controllers/meal/tests/mealDelete.spec.ts
+++ server/src/controllers/meal/tests/mealDelete.spec.ts
@@ -1,18 +1,24 @@
+import { authContext } from '@tests/utils/context'
 import { createTestDatabase } from '@tests/utils/database'
 import { createCallerFactory } from '@server/trpc'
+import { fakeUser } from '@server/entities/tests/fakes'
 import { wrapInRollbacks } from '@tests/utils/transactions'
 import { insertAll, clearTables, selectAll } from '@tests/utils/records'
 import mealRouter from '..'
 
 const db = await wrapInRollbacks(createTestDatabase())
 const createCaller = createCallerFactory(mealRouter)
-const { deleteMeal } = createCaller({ db })
+let user: any
 
 beforeEach(async () => {
-  await clearTables(db, ['meal'])
+  ;[user] = await insertAll(db, 'user', [fakeUser()])
 })
 
 it('should throw error if meal to delete is not found', async () => {
+  // arrange
+  const { deleteMeal } = createCaller(authContext({ db }, user))
+
+  // act & assert
   await expect(deleteMeal({ name: 'non-existing-meal' })).rejects.toThrowError()
 
   await insertAll(db, 'meal', { name: 'pizza', calories: 123 })
@@ -20,10 +26,14 @@ it('should throw error if meal to delete is not found', async () => {
 })
 
 it('should delete meal', async () => {
+  // arrange
+  await clearTables(db, ['meal'])
   await insertAll(db, 'meal', { name: 'pizza', calories: 123 })
   let meals = await selectAll(db, 'meal')
   expect(meals).toHaveLength(1)
+  const { deleteMeal } = createCaller(authContext({ db }, user))
 
+  // act & assert
   await expect(deleteMeal({ name: 'pizza' })).resolves.not.toThrowError()
   meals = await selectAll(db, 'meal')
   expect(meals).toHaveLength(0)
