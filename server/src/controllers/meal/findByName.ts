324f830cfbd58c9294fe4ace982e261102e672b3
KarolisG <kgaivenis@gmail.com>
2024-07-17 16:27:57 +0300

    update: meal controller find by name method - change to auth procedure

--- server/src/controllers/meal/findByName.ts
+++ server/src/controllers/meal/findByName.ts
@@ -1,10 +1,10 @@
-import { publicProcedure } from '@server/trpc'
+import { authenticatedProcedure } from '@server/trpc/authenticateProcedure'
 import { TRPCError } from '@trpc/server'
 import provideRepos from '@server/trpc/provideRepos'
 import { mealRepository } from '@server/repositories/mealRepository'
 import { mealSchema } from '@server/entities/meal'
 
-export default publicProcedure
+export default authenticatedProcedure
   .use(
     provideRepos({
       mealRepository,
--- server/src/controllers/meal/tests/mealFindByName.spec.ts
+++ server/src/controllers/meal/tests/mealFindByName.spec.ts
@@ -1,5 +1,6 @@
+import { authContext } from '@tests/utils/context'
 import { createTestDatabase } from '@tests/utils/database'
-import { fakeMeal } from '@server/entities/tests/fakes'
+import { fakeMeal, fakeUser } from '@server/entities/tests/fakes'
 import { createCallerFactory } from '@server/trpc'
 import { wrapInRollbacks } from '@tests/utils/transactions'
 import { insertAll } from '@tests/utils/records'
@@ -7,19 +8,42 @@ import mealRouter from '..'
 
 const db = await wrapInRollbacks(createTestDatabase())
 const createCaller = createCallerFactory(mealRouter)
+let user: any
 
-const [meal] = await insertAll(db, 'meal', [fakeMeal()])
-
-const { findByName } = createCaller({ db })
+beforeEach(async () => {
+  // await clearTables(db, ['meal'])
+  ;[user] = await insertAll(db, 'user', [fakeUser()])
+})
 
 it("should find meal by it's name", async () => {
+  // arrange
+  const [meal] = await insertAll(db, 'meal', [fakeMeal()])
+  const { findByName } = createCaller(authContext({ db }, user))
+
+  // act
   const result = await findByName({ name: meal.name })
 
+  // assert
   expect(result.name).toBe(meal.name)
 })
 
 it('throws error if meal is not found', async () => {
+  const { findByName } = createCaller(authContext({ db }, user))
   await expect(findByName({ name: 'non existing' })).rejects.toThrowError(
     /name not found/i
   )
+})
+
+it('prevents unauth user from using method', async () => {
+  // arrange
+  const { findAll } = createCaller({
+    db,
+    req: {
+      // no Auth header
+      header: () => undefined,
+    } as any,
+  })
+
+  // act & assert
+  await expect(findAll()).rejects.toThrowError(/unauthenticated/i)
 })
