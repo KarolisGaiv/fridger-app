88b7c3e86ef78c4e81411749fcf5febde68852e5
KarolisG <kgaivenis@gmail.com>
2024-07-17 16:57:19 +0300

    update: ingredient controller find by name method. Change to authenticated procedure

--- server/src/controllers/ingredient/tests/ingredientFindByName.spec.ts
+++ server/src/controllers/ingredient/tests/ingredientFindByName.spec.ts
@@ -1,5 +1,6 @@
+import { authContext } from '@tests/utils/context'
 import { createTestDatabase } from '@tests/utils/database'
-import { fakeIngredient } from '@server/entities/tests/fakes'
+import { fakeIngredient, fakeUser } from '@server/entities/tests/fakes'
 import { createCallerFactory } from '@server/trpc'
 import { wrapInRollbacks } from '@tests/utils/transactions'
 import { insertAll } from '@tests/utils/records'
@@ -7,18 +8,45 @@ import ingredientRouter from '..'
 
 const db = await wrapInRollbacks(createTestDatabase())
 const createCaller = createCallerFactory(ingredientRouter)
-const { findByName } = createCaller({ db })
+let user: any
+let ingredient: any
 
-const [ingredient] = await insertAll(db, 'ingredient', fakeIngredient())
+beforeEach(async () => {
+  ;[user] = await insertAll(db, 'user', [fakeUser()])
+  ;[ingredient] = await insertAll(db, 'ingredient', fakeIngredient())
+})
 
 it('should find ingredient by its name', async () => {
+  // arrange
+  const { findByName } = createCaller(authContext({ db }, user))
+
+  // act
   const res = await findByName({ name: ingredient.name })
 
+  // assert
   expect(res.name).toBe(ingredient.name)
 })
 
 it('throws error if ingredient is not found', async () => {
+  const { findByName } = createCaller(authContext({ db }, user))
+
   await expect(findByName({ name: 'non existing' })).rejects.toThrowError(
     /name not found/i
+  )
+})
+
+it('prevents unauth user from using method', async () => {
+  // arrange
+  const { create } = createCaller({
+    db,
+    req: {
+      // no Auth header
+      header: () => undefined,
+    } as any,
+  })
+
+  // act & assert
+  await expect(create(fakeIngredient())).rejects.toThrowError(
+    /unauthenticated/i
   )
 })
