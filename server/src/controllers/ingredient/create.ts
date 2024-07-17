9653b92f087cb8a0111f3e9c40753e25ed707783
KarolisG <kgaivenis@gmail.com>
2024-07-17 16:48:39 +0300

    update: ingredient controller update method. CHange to authenticated procedure

--- server/src/controllers/ingredient/create.ts
+++ server/src/controllers/ingredient/create.ts
@@ -1,11 +1,11 @@
-import { publicProcedure } from '@server/trpc'
+import { authenticatedProcedure } from '@server/trpc/authenticateProcedure'
 import { TRPCError } from '@trpc/server'
 import provideRepos from '@server/trpc/provideRepos'
 import { ingredientRepository } from '@server/repositories/ingredientRepository'
 import { assertError } from '@server/utils/errors'
 import { ingredientSchema } from '@server/entities/ingredient'
 
-export default publicProcedure
+export default authenticatedProcedure
   .use(
     provideRepos({
       ingredientRepository,
--- server/src/controllers/ingredient/tests/ingredientCreate.spec.ts
+++ server/src/controllers/ingredient/tests/ingredientCreate.spec.ts
@@ -1,28 +1,45 @@
+import { authContext } from '@tests/utils/context'
 import { createTestDatabase } from '@tests/utils/database'
-import { fakeIngredient } from '@server/entities/tests/fakes'
+import { fakeIngredient, fakeUser } from '@server/entities/tests/fakes'
 import { createCallerFactory } from '@server/trpc'
 import { wrapInRollbacks } from '@tests/utils/transactions'
-import { selectAll } from '@tests/utils/records'
+import { insertAll } from '@tests/utils/records'
 import ingredientRouter from '..'
 
 const db = await wrapInRollbacks(createTestDatabase())
 const createCaller = createCallerFactory(ingredientRouter)
-const { create } = createCaller({ db })
+let user: any
+
+beforeEach(async () => {
+  ;[user] = await insertAll(db, 'user', [fakeUser()])
+})
 
 it('should create and save new ingredient', async () => {
+  // arrange
   const ingredient = fakeIngredient()
+  const { create } = createCaller(authContext({ db }, user))
+
+  // act
   const res = await create(ingredient)
 
-  const [ingredientCreated] = await selectAll(db, 'ingredient', (query) =>
-    query('name', '=', ingredient.name)
+  // assert
+  expect(res.ingredientCreated).toMatchObject({
+    name: ingredient.name,
+  })
+})
+
+it('prevents unauth user from using method', async () => {
+  // arrange
+  const { create } = createCaller({
+    db,
+    req: {
+      // no Auth header
+      header: () => undefined,
+    } as any,
+  })
+
+  // act & assert
+  await expect(create(fakeIngredient())).rejects.toThrowError(
+    /unauthenticated/i
   )
-
-  expect(ingredientCreated).toMatchObject({
-    id: expect.any(Number),
-    ...ingredient,
-  })
-
-  expect(res.ingredientCreated).toEqual({
-    name: ingredientCreated.name,
-  })
 })
