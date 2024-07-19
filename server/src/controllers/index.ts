41ef4946b7f3df954575dca2a7e14a8963c0b8a8
KarolisG <kgaivenis@gmail.com>
2024-07-19 15:06:03 +0300

    chore: add grocery list and firdge content routes

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -3,12 +3,16 @@ import user from './user'
 import meal from './meal'
 import ingredient from './ingredient'
 import mealIngredient from './mealIngredient'
+import groceryList from './groceryList'
+import fridgeContent from './fridgeContent'
 
 export const appRouter = router({
   user,
   meal,
   ingredient,
   mealIngredient,
+  groceryList,
+  fridgeContent
 })
 
 export type AppRouter = typeof appRouter
