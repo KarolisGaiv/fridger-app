708a8885518bf791969263911b06141b6eb6ea06
KarolisG <kgaivenis@gmail.com>
2024-08-13 15:04:59 +0300

    updatE: client side code to export entities to fron end

--- server/src/shared/types.ts
+++ server/src/shared/types.ts
@@ -1,3 +1,11 @@
 // Only sharing the types!
 // Types are stripped out by the compiler, so this is safe.
 export type * from '../database/types'
+
+export type * from "../entities/fridgeContent"
+export type * from "../entities/groceryList"
+export type * from "../entities/ingredient"
+export type * from "../entities/meal"
+export type * from "../entities/mealIngredient"
+export type * from "../entities/mealPlan"
+export type * from "../entities/user"
