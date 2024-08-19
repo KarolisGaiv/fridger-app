99677ac280a7c96b5b8f3d88b8a705ca87e7239b
KarolisG <kgaivenis@gmail.com>
2024-08-19 16:12:38 +0300

    fix: formatting

--- client/src/views/AddMealPlan.vue
+++ client/src/views/AddMealPlan.vue
@@ -9,7 +9,7 @@ import AlertError from '@/components/AlertError.vue'
 const router = useRouter()
 
 const mealPlanForm = ref({
-  planName: ""
+  planName: '',
 })
 
 const successMessage = ref<string | null>(null)
@@ -19,10 +19,10 @@ const createdMealPlanId = ref<number | null>(null)
 const [createMealPlan, errorMessage] = useErrorMessage(async () => {
   const mealPlan = await trpc.mealPlan.create.mutate(mealPlanForm.value)
 
-  successMessage.value = "Meal plan created successfully!"
+  successMessage.value = 'Meal plan created successfully!'
   createdMealPlanId.value = mealPlan.id
 
-  mealPlanForm.value.planName = ""
+  mealPlanForm.value.planName = ''
 
   showOptions.value = true // Show options for adding meals or going to dashboard
 })
@@ -54,8 +54,8 @@ const goToDashboard = () => {
       </div>
     </div>
 
-     <!-- Show success message -->
-     <div v-if="successMessage" class="mt-6 text-green-600">
+    <!-- Show success message -->
+    <div v-if="successMessage" class="mt-6 text-green-600">
       {{ successMessage }}
     </div>
 
@@ -67,8 +67,8 @@ const goToDashboard = () => {
   </form>
 
   <div v-if="showOptions" class="mt-6 space-x-4">
-      <p>Would you like to add meals to this plan?</p>
-      <FwbButton size="lg" @click="addMeals">Yes, add meals</FwbButton>
-      <FwbButton size="lg" @click="goToDashboard">No, go to dashboard</FwbButton>
-    </div>
+    <p>Would you like to add meals to this plan?</p>
+    <FwbButton size="lg" @click="addMeals">Yes, add meals</FwbButton>
+    <FwbButton size="lg" @click="goToDashboard">No, go to dashboard</FwbButton>
+  </div>
 </template>
