c8b4454f90adc7b4ec43b0bcc97baef36eeccbb4
KarolisG <kgaivenis@gmail.com>
2024-08-23 10:02:25 +0300

    update: add meal view. add fields to assign day and meal type to new meal

--- client/src/views/AddMeal.vue
+++ client/src/views/AddMeal.vue
@@ -1,21 +1,50 @@
 <script setup lang="ts">
 import { trpc } from '@/trpc'
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
-import { FwbButton, FwbHeading, FwbInput } from 'flowbite-vue'
+import { FwbButton, FwbHeading, FwbInput, FwbSelect } from 'flowbite-vue'
 import useErrorMessage from '@/composables/useErrorMessage'
 import AlertError from '@/components/AlertError.vue'
 
 const router = useRouter()
+const activePlan = ref("")
+const availablePlans = ref([])
 
 // State for Meal form
 const mealForm = ref({
   name: '',
   calories: '0',
+  mealPlan: activePlan.value ? activePlan.value : null,
+  assignedDay: 1,
+  type: ""
 })
+
+const planDays = [
+  {value: 1, name: "Day 1"},
+  {value: 2, name: "Day 2"},
+  {value: 3, name: "Day 3"},
+  {value: 4, name: "Day 4"},
+  {value: 5, name: "Day 5"},
+  {value: 6, name: "Day 6"},
+  {value: 7, name: "Day 7"},
+  {value: null, name: "No assigned day"},
+]
+
+const mealTypes = [
+  {value: "breakfast", name: "Breakfast"},
+  {value: "lunch", name: "Lunch"},
+  {value: "dinner", name: "Dinner"},
+  {value: "snack", name: "Snack"},
+]
 
 const successMessage = ref<string | null>(null)
 const showOptions = ref(false) // Show options after meal is added
+
+onMounted(async () => {
+  activePlan.value = await trpc.mealPlan.findActiveMealPlan.query()
+})
+
+
 
 const [createMeal, errorMessage] = useErrorMessage(async () => {
   const formData = {
@@ -43,7 +72,7 @@ const goToDashboard = () => {
 
 <template>
   <div class="space-y-6">
-    <FwbHeading tag="h1" class="text-3xl">Add a meal to your plan</FwbHeading>
+    <FwbHeading tag="h1" class="text-3xl">Add meal to your plan</FwbHeading>
 
     <form aria-label="Add Meal" @submit.prevent="createMeal">
       <div class="mt-6">
@@ -64,10 +93,25 @@ const goToDashboard = () => {
           placeholder="Calories"
         />
       </div>
+      <div class="mt-6">
+        <fwb-select
+        v-model="mealForm.assignedDay"
+        :options="planDays"
+        label="Assign to"
+        />
+      </div>
+      <div class="mt-6">
+        <fwb-select
+        v-model="mealForm.type"
+        :options="mealTypes"
+        label="Meal type"
+        />
+      </div>
 
       <AlertError :message="errorMessage" />
 
-      <div class="mt-6 flex justify-end">
+      <div class="mt-6 flex justify-between">
+        <div class="mt-6">Active meal plan: {{ activePlan }}</div>
         <FwbButton size="lg" type="submit">Add meal</FwbButton>
       </div>
     </form>
