d29d3b0980da9e2a458d30d678e8e6cc7ad4055b
KarolisG <kgaivenis@gmail.com>
2024-08-29 14:40:50 +0300

    chore: delete unused code

--- client/src/views/FridgeView.vue
+++ client/src/views/FridgeView.vue
@@ -12,20 +12,11 @@ const fridgeItems = ref<any[]>([])
 onMounted(async () => {
   fridgeItems.value = await trpc.fridgeContent.findByUser.query()
 })
-
-const testAI = async () => {
-  const test = await trpc.aiBot.create.query()
-  console.log(test)
-}
 </script>
 
 <template>
   <div class="space-y-6">
     <FwbHeading tag="h1" class="text-3xl">Fridge Content</FwbHeading>
-  </div>
-
-  <div class="mt-6">
-    <FwbButton size="lg" @click="testAI"> TEST </FwbButton>
   </div>
 
   <div class="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
@@ -36,6 +27,4 @@ const testAI = async () => {
       :currentQuantity="item.existingQuantity"
     />
   </div>
-
-  <!-- <AlertError :message="errorMessage" /> -->
 </template>
