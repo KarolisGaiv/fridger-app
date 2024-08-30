ef7e2d97c3ddb9efa560b9d692370789d9c9aec2
KarolisG <kgaivenis@gmail.com>
2024-08-30 14:20:03 +0300

    test: E2E, add ingredients to meal flow

--- client/e2e/test-1.spec.ts
+++ client/e2e/test-1.spec.ts
@@ -96,6 +96,43 @@ test.describe.serial("new user journey", () => {
   await expect(page.getByRole('heading', { name: 'test meal' })).toBeVisible()
   await expect(page.getByText('Not Completed')).toBeVisible()
 })
+
+  test("add ingredients to meal", async({page}) => {
+    await page.goto('http://localhost:5173/');
+    await page.goto('http://localhost:5173/login');
+    await page.locator('input[type="email"]').click();
+    await page.locator('input[type="email"]').fill(user.email);
+    await page.locator('input[type="email"]').press('Tab');
+    await page.locator('input[name="password"]').fill(user.password);
+    await page.getByRole('button', { name: 'Log in' }).click();
+
+    await page.getByRole('link', { name: 'Edit Meal Plan' }).click();
+    await page.getByRole('button', { name: 'Add Ingredients' }).click();
+    await expect(page.getByRole('heading', { name: 'Add Ingredients To Meal' })).toBeVisible()
+    await expect(page.getByText('Select Meal')).toBeVisible()
+    await page.getByLabel('Select Meal').selectOption('test meal');
+
+    await expect(page.getByRole('button', { name: 'Select existing ingredient' })).toBeVisible()
+    await expect(page.getByRole('button', { name: 'Add new ingredient' })).toBeVisible()
+
+    await page.getByRole('button', { name: 'Add new ingredient' }).click();
+    
+    await expect(page.locator('label').filter({ hasText: 'Add New Ingredient' })).toBeVisible()
+    await expect(page.getByText('Quantity')).toBeVisible()
+
+    await page.getByPlaceholder('Add new ingredient').click();
+    await page.getByPlaceholder('Add new ingredient').fill('test ingredient');
+    await page.getByRole('spinbutton', { name: 'Quantity' }).click();
+    await page.getByRole('spinbutton', { name: 'Quantity' }).fill('123');
+    await page.getByRole('button', { name: 'Add New Ingredient', exact: true }).click();
+
+    await expect(page.getByText('test ingredient added to \'')).toBeVisible()
+
+    await page.getByRole('button', { name: 'Show Ingredients' }).click();
+
+    await expect(page.locator('div').filter({ hasText: 'test ingredient' }).nth(3)).toBeVisible()
+    await expect(page.getByRole('heading', { name: 'test ingredient' })).toBeVisible()
+  })
 })
 
 
