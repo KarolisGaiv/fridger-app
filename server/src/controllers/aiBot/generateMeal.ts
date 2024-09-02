21745450c7686fbcb47a61f3a64efb2fc6af8feb
KarolisG <kgaivenis@gmail.com>
2024-09-02 11:38:08 +0300

    chore: fix server lint and typecheck errors. add github flow actions

--- server/src/controllers/aiBot/generateMeal.ts
+++ server/src/controllers/aiBot/generateMeal.ts
@@ -4,7 +4,7 @@ import { mealSchema } from '@server/entities/meal'
 
 export default authenticatedProcedure
   .input(mealSchema.pick({ type: true, calories: true }))
-  .query(async ({ input, ctx: { authUser } }) => {
+  .query(async ({ input }) => {
     const { type, calories } = input
 
     const result = await generateAIMealBy(type!, calories)
--- server/src/services/aiBot.ts
+++ server/src/services/aiBot.ts
@@ -24,39 +24,6 @@ const MealPlanSchema = z.object({
   ),
 })
 
-export async function generateAIResponse(fridgeContent, preferences) {
-  // Convert fridgeContent and preferences to strings
-  const fridgeContentString = JSON.stringify(fridgeContent).replace(/"/g, '\\"')
-
-  const preferencesString = JSON.stringify(preferences).replace(/"/g, '\\"')
-
-  const completion = await openai.chat.completions.create({
-    model: 'gpt-4o-mini',
-    messages: [
-      {
-        role: 'system',
-        content:
-          "You are a meal planner. Given a list of ingredients in the user's fridge, generate meal suggestions that only use those ingredients. Ensure that all quantities are provided in metric units (grams, liters, milliliters). Respond with only the generated meal plans in the specified JSON format.",
-      },
-      {
-        role: 'user',
-        content: `Ingredients in my fridge: ${fridgeContentString}. I would like to have the following meals: ${preferencesString}. Suggest meals using only these ingredients.`,
-      },
-    ],
-    response_format: zodResponseFormat(MealPlanSchema, 'meal_plans'),
-  })
-
-  const mealPlans = completion.choices[0].message
-
-  if (mealPlans.refusal) {
-    return mealPlans.refusal
-  }
-
-  // Parse the response content to JSON
-  const jsonResponse = JSON.parse(mealPlans.content!)
-  return jsonResponse
-}
-
 export async function generateAIMealBy(mealType: string, calories: number) {
   const completion = await openai.chat.completions.create({
     model: 'gpt-4o-mini',
