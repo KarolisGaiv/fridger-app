4dce31995605a2dcef6ee99fa941d0f88d79ee4b
KarolisG <kgaivenis@gmail.com>
2025-01-21 14:16:28 +0200

    stlye: navigation  componene

--- client/src/app/components/Navigation.tsx
+++ client/src/app/components/Navigation.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 const Navigation = () => {
   return (
-    <nav>
+    <nav className="flex justify-around bg-green-400 py-8">
       <Link href="/add-meal-plan">Add Meal Plan</Link>
       <Link href="/edit-meal-plan">Edit Meal Plan</Link>
       <Link href="/grocery-list">Grocery List</Link>
---
+++ client/src/app/plan-details/page.tsx
@@ -0,0 +1,9 @@
+'use-client'
+
+export default function PlanDetails() {
+  return (
+    <div>
+      <h1>Edit Meal MealPlanRepository</h1>
+    </div>
+  )
+}
