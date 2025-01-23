cdf16b1c931b70007cdbf2d0a70809e4c536499c
KarolisG <kgaivenis@gmail.com>
2025-01-23 13:41:15 +0200

    style: header component

--- client/src/layouts/MainLayout.tsx
+++ client/src/layouts/MainLayout.tsx
@@ -2,27 +2,36 @@ import { Link, Outlet } from 'react-router-dom'
 
 const MainLayout = () => {
   return (
-    <div className="main-layout">
-      <header>
-        <nav>
-          <ul>
+    <div className="main-layout flex min-h-screen flex-col">
+      <header className="bg-blue-500 py-4 text-white">
+        <nav className="container mx-auto flex justify-center">
+          <ul className="flex space-x-4">
             <li>
-              <Link to="/add-meal-plan">Add Meal Plan</Link>
+              <Link to="/add-meal-plan" className="hover:text-gray-300">
+                Add Meal Plan
+              </Link>
+            </li>
+
+            <li>
+              <Link to="/add-meal" className="hover:text-gray-300">
+                Add Meal
+              </Link>
             </li>
             <li>
-              <Link to="/add-meal">Add Meal</Link>
+              <Link to="/grocery-list" className="hover:text-gray-300">
+                Grocery List
+              </Link>
             </li>
             <li>
-              <Link to="/grocery-list">Grocery List</Link>
-            </li>
-            <li>
-              <Link to="/fridge">Fridge</Link>
+              <Link to="/fridge" className="hover:text-gray-300">
+                Fridge
+              </Link>
             </li>
           </ul>
         </nav>
       </header>
 
-      <main>
+      <main className="container mx-auto mt-4 flex-1 p-4">
         <Outlet />
       </main>
     </div>
--- client/src/router/guards.tsx
+++ client/src/router/guards.tsx
@@ -3,6 +3,7 @@ import { useUserStore } from '@/stores/user'
 
 export const RequireAuth = ({ children }: { children: React.ReactNode }) => {
   const isLoggedIn = useUserStore((state) => state.isLoggedIn)
+  console.log(isLoggedIn)
 
   if (!isLoggedIn) {
     return <Navigate to="/login" replace />
