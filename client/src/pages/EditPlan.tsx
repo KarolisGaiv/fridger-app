271e9028bc4a59804ecf6e81478de8b23dba097c
KarolisG <kgaivenis@gmail.com>
2025-01-24 10:29:32 +0200

    style: add init styling to edit meal plan page

--- client/src/pages/EditPlan.tsx
+++ client/src/pages/EditPlan.tsx
@@ -7,12 +7,11 @@ export default function EditPlanPage() {
   const { plannedMeals } = usePlannedMeals()
 
   return (
-    <div>
-      <h1>Edit Your Plan Here</h1>
-      <h2>Active Meal Plan: {activePlan}</h2>
+    <div className="container mx-auto p-6">
+      <h1 className="mb-4 text-2xl font-bold">Edit Your Plan Here</h1>
+      <h2 className="mb-4 text-xl font-medium">Active Meal Plan: {activePlan}</h2>
 
-      <></>
-      <h3>Your plan</h3>
+      <h3 className="mb-4 text-lg font-semibold">Your Weekly Plan</h3>
       <MealCalendar plannedMeals={plannedMeals} />
     </div>
   )
