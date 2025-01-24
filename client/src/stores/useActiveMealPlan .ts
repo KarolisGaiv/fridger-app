8662e8a9679b5b10e9dad96e007eb8bbeb2a825f
KarolisG <kgaivenis@gmail.com>
2025-01-24 09:53:05 +0200

    feat: adjust useActiveMealPlan hook to just read data from user store

--- client/src/stores/useActiveMealPlan .ts
+++ client/src/stores/useActiveMealPlan .ts
@@ -1,20 +1,6 @@
 import { useUserStore } from '@/stores/user'
-import { useEffect, useState } from 'react'
-import type { UserState } from '@/stores/user'
 
 export default function useActiveMealPlan() {
-  const [loading, setLoading] = useState(true)
-  const activePlan = useUserStore((state: UserState) => state.activePlan)
-  const getActiveMealPlan = useUserStore((state) => state.getActiveMealPlan)
-
-  useEffect(() => {
-    const fetchPlan = async () => {
-      await getActiveMealPlan()
-      setLoading(false)
-    }
-    fetchPlan()
-  }, [getActiveMealPlan])
-
-  if (loading) return { loading: true, activePlan: null }
-  return { loading: false, activePlan }
+  const activePlan = useUserStore((state) => state.activePlan)
+  return { activePlan }
 }
